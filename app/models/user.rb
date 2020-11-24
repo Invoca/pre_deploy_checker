c8f4c82e30722d706a030225b8c6f67d0ded616d
James Ebentier <jebentier@invoca.com>
2020-11-24 16:50:30 +0100

    No-Jira: clean up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,29 @@
 class User < ActiveRecord::Base
-  include GitModels::User
+  fields do
+    name  :string, limit: 255, index: true, unique: true, validates: { uniqueness: { scope: :email } }
+    email :string, limit: 255
+
+    timestamps
+  end
+
+  has_many :branches, class_name: 'Branch',    foreign_key: 'author_id',   inverse_of: :author
+  has_many :commits,  class_name: 'Commit',    foreign_key: 'author_id',   inverse_of: :author
 
   class << self
+    def users_with_emails(email_filter_list)
+      # if filter is empty, return all users, otherwise only return users whose emails are in the list
+      if email_filter_list.empty?
+        all
+      else
+        all.select { |user| email_filter_list.include?(user.email.downcase) }
+      end
+    end
+
+    # TODO: refactor this out
     def create_from_jira_data!(jira_user_data)
       # Uses fake email to guarantee uniqueness
       name = jira_user_data.displayName
-      User.where(name: name, email: fake_email_from_name(name)).first_or_create!
+      User.find_or_create_by!(name: github_data.author_name, email: github_data.author_email)
     end
 
     private
