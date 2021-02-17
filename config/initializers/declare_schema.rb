b8355e9037a9db9ea1f6e06745492f46ae07b431
Colin Kelley <colindkelley@gmail.com>
2021-02-16 16:18:37 -0800

    TECH-4814: configure declare_schema with default_charset and default_collation

---
+++ config/initializers/declare_schema.rb
@@ -0,0 +1,4 @@
+# froze_string_literal: true
+
+Generators::DeclareSchema::Migration::Migrator.default_charset   = "utf8"
+Generators::DeclareSchema::Migration::Migrator.default_collation = "utf8_general_ci"
